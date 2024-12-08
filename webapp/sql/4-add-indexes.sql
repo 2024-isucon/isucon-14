4ccd13ddf9a0caedc96f52360be5ef4a1331a1c3
TAKAHiRO TOMiNAGA <52474650+kokoichi206@users.noreply.github.com>
2024-12-08 15:38:50 +0900

    add: index to ride_statuses

--- webapp/sql/4-add-indexes.sql
+++ webapp/sql/4-add-indexes.sql
@@ -1,1 +1,3 @@
 CREATE INDEX chair_locations_chair_id_created_at_idx  ON chair_locations(chair_id, created_at);
+
+CREATE INDEX idx_ride_statuses_ride_id_created_at ON ride_statuses(ride_id, created_at);
